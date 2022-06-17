dcf449de3a444c4a79be5be853ac075faed9a9ef
seomuyeol <muyear.seo@gmail.com>
2022-06-17 11:42:48 +0900

    AccountService Test 作成

---
+++ src/test/java/jp/winschool/spring/jobboard/service/AccountServiceTest.java
@@ -0,0 +1,74 @@
+package jp.winschool.spring.jobboard.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import jp.winschool.spring.jobboard.model.Account;
+import jp.winschool.spring.jobboard.model.Company;
+import jp.winschool.spring.jobboard.repository.AccountRepository;
+
+@SpringJUnitConfig
+class AccountServiceTest {
+	
+	@TestConfiguration
+	static class Config {
+		@Bean
+		public AccountService accountService() {
+			return new AccountService();
+		}
+	}
+
+	@Autowired
+	private AccountService accountService;
+	
+	@MockBean
+	private AccountRepository accountRepository;
+	
+	@MockBean
+    private CompanyService companyService;
+    
+    @MockBean
+    private PersonService personService;
+    
+    @MockBean
+    private SpringUserService springUserService;
+	
+	@Test
+	void testFind() {
+		Account account = new Account();
+		
+		when(accountRepository.findById("win")).thenReturn(Optional.of(account));
+		
+		Account a = accountService.find("win");
+		verify(accountRepository).findById("win");
+	}
+	
+	@Test
+	public void testCreateAdministratorAccount() {
+		Account account = accountService.createAdministratorAccount("win", "win", true);
+		verify(accountRepository).save(account);
+		verify(springUserService).createSpringUser("win", "win", "ADMINISTRATOR", true);
+	}
+
+	@Test
+	void testCreateCompanyAccount() {
+		Company company = new Company();
+		when(companyService.createDefaultValueCompany()).thenReturn(company);
+		
+		Account account = accountService.createCompanyAccount("win", "win", true);
+		assertThat(account.getCompany()).isNotNull();
+		verify(accountRepository).save(account);
+		verify(companyService).createDefaultValueCompany();
+        verify(springUserService).createSpringUser("win", "win", "COMPANY", true);
+	}
+}
