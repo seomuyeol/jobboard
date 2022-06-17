a37f66a01ddd0465a5fd40e83bb9d67e69798add
seomuyeol <muyear.seo@gmail.com>
2022-06-17 13:48:23 +0900

    Controller commit

--- src/main/java/jp/winschool/spring/jobboard/JobboardApplication.java
+++ src/main/java/jp/winschool/spring/jobboard/JobboardApplication.java
@@ -1,13 +1,41 @@
 package jp.winschool.spring.jobboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.dao.DuplicateKeyException;
+
+import jp.winschool.spring.jobboard.service.AccountService;
 
 @SpringBootApplication
-public class JobboardApplication {
+public class JobboardApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobboardApplication.class, args);
 	}
+	
+    // JobboardApplicationは@DataJpaTestを付けたテストでもインスタンス化されるが
+    // その場合AccountServiceインスタンスがないのでエラーになる。
+    // 「required = false」とするとインスタンスがない場合でもエラーにならない。
+    @Autowired(required = false)
+    private AccountService accountService;
+    
+    @Override
+    public void run(String... args) throws Exception {
+    	//インスタンスがDIされていない（テスト時）は実行しない
+    	if (accountService == null) {
+    		return;
+    	}
+    	
+    	String adminUsername = "admin";
+    	String adminPassword = "admin";
+    	
+    	try { 
+    		accountService.createAdministratorAccount(adminUsername, adminPassword, true);
+    	} catch (DuplicateKeyException e) {
+    	}
+    }
+    
 
 }
---
+++ src/main/java/jp/winschool/spring/jobboard/controller/HomeController.java
@@ -0,0 +1,18 @@
+package jp.winschool.spring.jobboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home(Model model) {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
