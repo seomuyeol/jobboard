ab4f5b7fb977508ad8b69e2c8f492cd55b52a90c
seomuyeol <muyear.seo@gmail.com>
2022-06-20 15:34:27 +0900

    AccountServiceTest modified

--- src/test/java/jp/winschool/spring/jobboard/service/AccountServiceTest.java
+++ src/test/java/jp/winschool/spring/jobboard/service/AccountServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -44,6 +45,12 @@ class AccountServiceTest {
     @MockBean
     private SpringUserService springUserService;
 	
+    @Test
+    public void testGetAccountList() {
+        List<Account> accounts = accountService.getAccountList();
+        verify(accountRepository).findAll();
+    }
+    
 	@Test
 	void testFind() {
 		Account account = new Account();
